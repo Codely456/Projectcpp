d0588c533652aef2a4a00dda46370e1ec3468598
Codely456 <theb12969@gmail.com>
2025-02-18 19:11:27 +0530

    The operator changed on both side of equation from +(postive) to -(negative) check it out:

--- check3.c
+++ check3.c
@@ -4,7 +4,7 @@ int main()
 	int a,b,c,d;
 	printf("Enter values of a,b,c,d:");
 	scanf("%d %d %d %d",&a,&b,&c,&d);
-	printf("Result: %d\n",a + b == c + d);
+	printf("Result: %d\n",a - b == c - d);
 	return 0;
 
 }

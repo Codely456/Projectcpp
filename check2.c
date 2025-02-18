153cd63c4d674cb92d063081547487fc274438a3
Codely456 <theb12969@gmail.com>
2025-02-18 12:50:39 +0530

    The new variable d is added and new function too check it outgit add check2.c

--- check2.c
+++ check2.c
@@ -1,10 +1,10 @@
 #include<stdio.>
 int main()
 {
-	int a,b,c;
-	printf("Enter values of a,b,c:");
-	scanf("%d %d %d",a,b,c);
-	printf("Result: %d\n",a + b ==c);
+	int a,b,c,d;
+	printf("Enter values of a,b,c,d:");
+	scanf("%d %d %d",a,b,c,d);
+	printf("Result: %d\n",a + b ==c + d);
 	return 0;
 
 }

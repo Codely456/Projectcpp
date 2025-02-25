814154641c3584586a8657eb510b0ea215ab9938
Codely456 <theb12969@gmail.com>
2025-02-25 11:52:11 +0530

    Added loop to check multiple ages without restarting the program

--- check6.c
+++ check6.c
@@ -2,7 +2,10 @@
 
 int main() {
     int age;
-    
+    char choice;
+
+    do {
+   
     // Taking user input for age
     printf("Enter your age: ");
     
@@ -24,6 +27,12 @@ int main() {
         printf("Sorry, you are not eligible to vote.\n");
         printf("You can vote after %d years.\n", years_left);
     }
+
+     // Ask if the user wants to check again
+        printf("Do you want to check another age? (y/n): ");
+        scanf(" %c", &choice);  // Space before %c to consume newline character
+
+    } while (choice == 'y' || choice == 'Y');
 
     // Thank you message
     printf("Thank you for checking your voting eligibility!\n");

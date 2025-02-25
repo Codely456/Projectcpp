0a146def648f8b87cae4004d478f97c1f10c5b05
Codely456 <theb12969@gmail.com>
2025-02-25 11:26:16 +0530

    added additional messages to encourage voting and inform younger users

--- check5.c
+++ check5.c
@@ -1,19 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
     int age;
-
+    
     // Taking user input for age
     printf("Enter your age: ");
     scanf("%d", &age);
-
+    
     // Checking voting eligibility
     if (age >= 18) {
-        printf("You are eligible to vote.\n");
+        printf("You are eligible to vote!\n");
+        printf("Make sure to participate in the elections.\n");
     } else {
-        printf("You are not eligible to vote.\n");
+        printf("Sorry, you are not eligible to vote.\n");
+        printf("You can vote once you turn 18.\n");
     }
-
+    
     return 0;
 }
+
 
